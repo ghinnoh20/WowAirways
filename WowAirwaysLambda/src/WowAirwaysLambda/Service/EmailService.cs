2064b96e47fb1ca6ad2b59c88ad1484d07d530d9
Gino Ingreso <geingreso@shakeys.biz>
2024-02-01 10:27:03 +0800

    Added a booking template and a draft email service

---
+++ WowAirwaysLambda/src/WowAirwaysLambda/Service/EmailService.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace WowAirwaysLambda.Service
+{
+    public class EmailService
+    {
+        public void Send(string recipientEmail)
+        {
+            // Sender's email address and credentials
+            string senderEmail = "ginosoftwareengineermanager@gmail.com";
+            string senderPassword = "Cheese1!";
+
+            // Create the MailMessage object
+            MailMessage mail = new MailMessage(senderEmail, recipientEmail);
+            mail.Subject = "Hello, this is a test email";
+            mail.Body = "This is the body of the email.";
+
+            // Create the SmtpClient object
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+            smtpClient.Port = 587;
+            smtpClient.Credentials = new NetworkCredential(senderEmail, senderPassword);
+            smtpClient.EnableSsl = true;
+
+        }
+
+    }
+}
