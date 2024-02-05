7c398691fd9447ab3a136c05c962d5fefd90a21d
Gino Ingreso <geingreso@shakeys.biz>
2024-02-05 16:02:06 +0800

    Creation of PDF is now working

--- WowAirwaysPrinter/Form1.cs
+++ WowAirwaysPrinter/Form1.cs
@@ -1,25 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using WowAirwaysPrinter.Models;
+using WowAirwaysPrinter.Services;
 
 namespace WowAirwaysPrinter
 {
     public partial class Form1 : Form
     {
+
+        private PdfService _pdfService;
+
         public Form1()
         {
             InitializeComponent();
+
+            _pdfService = new PdfService();
         }
+
+        private void Log(string message)
+        {
+            rtxtStatus.AppendText($"[{DateTime.Now.ToString("hh:mm:ss:fff TT")}]{message}{Environment.NewLine}");
+        }
+
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Log("Creating PDFs...");
+                _pdfService.CreateBoardingPass("Linda, Blair 2", "23", BoardingPassType.Shakeys);
 
+
+
+                Log("PDFs created.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
--- WowAirwaysPrinter/Services/PdfService.cs
+++ WowAirwaysPrinter/Services/PdfService.cs
@@ -1,5 +1,6 @@
 ﻿using iText.Forms;
 using iText.Forms.Fields;
+using iText.IO.Source;
 using iText.Kernel.Pdf;
 using System.Collections.Generic;
 using System.IO;
@@ -9,18 +10,55 @@ namespace WowAirwaysPrinter.Services
 {
     public class PdfService
     {
+        private string GetTempalte(BoardingPassType boardingPassType)
+        {
+            var output = @"Templates/";
+
+            switch (boardingPassType)
+            {
+                case BoardingPassType.Shakeys:
+                    output += "Shakeys.pdf";
+                    break;
+                case BoardingPassType.PeriPeri:
+                    output += "PeriPeri.pdf";
+                    break;
+                case BoardingPassType.PotatoCorner:
+                    output += "PC-RB.pdf";
+                    break;
+                case BoardingPassType.RnB:
+                    output += "PC-RB.pdf";
+                    break;
+                default:
+                    output += "Default.pdf";
+                    break;
+            }
+
+            return output;
+        }
+
+        private void CreatePdf(byte[] bytes, string fileName)
+        {
+            if (!Directory.Exists(@"BoardingPassPDFs"))
+            {
+                Directory.CreateDirectory(@"BoardingPassPDFs");
+            }
+
+            using (FileStream fs = File.Create($@"BoardingPassPDFs/{fileName}")) { 
+                fs.Write(bytes, 0, (int)bytes.Length); 
+            }
+        }
+
         /// <summary>
         ///     Creates a boarding pass PDF file
         /// </summary>
         /// <param name="attendeeName">Name of the attendee to be printed. Format: Lastname, Firstname</param>
         /// <param name="seatNo">Seat number to be printed</param>
         /// <returns></returns>
-        public byte[] CreateBoardingPass(string attendeeName
+        public void CreateBoardingPass(string attendeeName
             , string seatNo
             , BoardingPassType boardingPassType = BoardingPassType.Default)
         {
-            string path = @"Templates/ItineraryTemplate.pdf";
-            byte[] output = null;
+            string path = GetTempalte(boardingPassType);
 
             if (System.IO.File.Exists(path))
             {
@@ -32,33 +70,29 @@ namespace WowAirwaysPrinter.Services
 
                 var form = PdfAcroForm.GetAcroForm(pdf, false);
 
-
                 if (form != null)
                 {
-
                     IDictionary<string, PdfFormField> fields = form.GetAllFormFields();
                     PdfFormField toSet;
 
-                    fields.TryGetValue("txtBookingReference", out toSet);
-                    toSet.SetValue(bookingReference);
+                    fields.TryGetValue("txtAttendee1", out toSet);
+                    toSet.SetValue(attendeeName);
 
-                    fields.TryGetValue("txtFlightNo1", out toSet);
-                    toSet.SetValue(flightNo);
+                    fields.TryGetValue("txtAttendee2", out toSet);
+                    toSet.SetValue(attendeeName);
 
-                    fields.TryGetValue("txtFlightNo2", out toSet);
-                    toSet.SetValue(flightNo);
+                    fields.TryGetValue("txtSeat1", out toSet);
+                    toSet.SetValue(seatNo);
 
-                    fields.TryGetValue("txtName", out toSet);
-                    toSet.SetValue($"{lastName},{firstName}");
+                    fields.TryGetValue("txtSeat2", out toSet);
+                    toSet.SetValue(seatNo);
 
                     pdf.Close();
 
-                    output = outputStream.ToArray();
+                    CreatePdf(outputStream.ToArray(), $"{attendeeName}-{boardingPassType.ToString()}.pdf");
                 }
 
             }
-
-            return output;
 
         }
     }
