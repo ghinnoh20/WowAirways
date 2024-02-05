028b7261e68d3820beeaaff43bb1b53b5eefdc93
Gino Ingreso <geingreso@shakeys.biz>
2024-02-05 15:29:42 +0800

    Created a draft service and enum to identify what brand of boarding pass to be printed

---
+++ WowAirwaysPrinter/Models/BoardingPassType.cs
@@ -0,0 +1,11 @@
+﻿namespace WowAirwaysPrinter.Models
+{
+    public enum BoardingPassType
+    {
+        Shakeys = 1,
+        PeriPeri = 2,
+        PotatoCorner = 3,
+        RnB = 4,
+        Default = 5
+    }
+}
---
+++ WowAirwaysPrinter/Services/PdfService.cs
@@ -0,0 +1,65 @@
+﻿using iText.Forms;
+using iText.Forms.Fields;
+using iText.Kernel.Pdf;
+using System.Collections.Generic;
+using System.IO;
+using WowAirwaysPrinter.Models;
+
+namespace WowAirwaysPrinter.Services
+{
+    public class PdfService
+    {
+        /// <summary>
+        ///     Creates a boarding pass PDF file
+        /// </summary>
+        /// <param name="attendeeName">Name of the attendee to be printed. Format: Lastname, Firstname</param>
+        /// <param name="seatNo">Seat number to be printed</param>
+        /// <returns></returns>
+        public byte[] CreateBoardingPass(string attendeeName
+            , string seatNo
+            , BoardingPassType boardingPassType = BoardingPassType.Default)
+        {
+            string path = @"Templates/ItineraryTemplate.pdf";
+            byte[] output = null;
+
+            if (System.IO.File.Exists(path))
+            {
+                var soruceFileStream = File.OpenRead(path);
+                var outputStream = new MemoryStream();
+
+                var pdf = new PdfDocument(new PdfReader(soruceFileStream)
+                    , new PdfWriter(outputStream));
+
+                var form = PdfAcroForm.GetAcroForm(pdf, false);
+
+
+                if (form != null)
+                {
+
+                    IDictionary<string, PdfFormField> fields = form.GetAllFormFields();
+                    PdfFormField toSet;
+
+                    fields.TryGetValue("txtBookingReference", out toSet);
+                    toSet.SetValue(bookingReference);
+
+                    fields.TryGetValue("txtFlightNo1", out toSet);
+                    toSet.SetValue(flightNo);
+
+                    fields.TryGetValue("txtFlightNo2", out toSet);
+                    toSet.SetValue(flightNo);
+
+                    fields.TryGetValue("txtName", out toSet);
+                    toSet.SetValue($"{lastName},{firstName}");
+
+                    pdf.Close();
+
+                    output = outputStream.ToArray();
+                }
+
+            }
+
+            return output;
+
+        }
+    }
+}
