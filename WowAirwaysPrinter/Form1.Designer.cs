821a02244a1d101aff099379fea7f44265aa6052
Gino Ingreso <geingreso@shakeys.biz>
2024-02-05 11:39:20 +0800

    Form design checkin

--- WowAirwaysPrinter/Form1.Designer.cs
+++ WowAirwaysPrinter/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.rtxtStatus = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // rtxtStatus
+            // 
+            this.rtxtStatus.Location = new System.Drawing.Point(32, 28);
+            this.rtxtStatus.Name = "rtxtStatus";
+            this.rtxtStatus.ReadOnly = true;
+            this.rtxtStatus.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.rtxtStatus.Size = new System.Drawing.Size(923, 552);
+            this.rtxtStatus.TabIndex = 0;
+            this.rtxtStatus.Text = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(759, 600);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(196, 66);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(996, 678);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.rtxtStatus);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -44,6 +68,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtStatus;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
